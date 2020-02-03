3c6edcdb5c80a40d2241854781124b87e40cb5fc
johnathanlo <49450170+johnathanlo@users.noreply.github.com>
2020-02-02 20:52:03 -0600

    Add files via upload

--- R/ReadFasta.R
+++ R/ReadFasta.R
@@ -1,12 +1,12 @@
 library(Rcpp)
-##sourceCpp("../microsatver3g.cpp")
-##source("str_rev.R")
+##sourceCpp("src/microsat_v4.cpp")
+##source("/str_rev.R")
 ##source("str_comp.R")
 ##source("FindXmers.R")
 
-ReadFasta <- function(file, minrepeats = c(6,4,3,3,3), squishy = c(0,0,0,0,0)){#minrepeats&squishy = vectors of values corresponding to two, three, four, five, and six-mers respectively
-  if (file.exists(file) && length(minrepeats) == 5 && length(squishy) == 5 && class(minrepeats) == "numeric" && class(squishy) == "numeric"){
-    x<-findMS(file, minrepeats, squishy)
+ReadFasta2 <- function(file, xmer, minrep, tolfac){#minrepeats&squishy = vectors of values corresponding to two, three, four, five, and six-mers respectively
+  if (file.exists(file) && length(minrep) == 1 && length(tolfac) == 1 && class(minrep) == "numeric" && class(tolfac) == "numeric"){
+    x<-FindMS2(file, xmer, minrep, tolfac)
     return(x)
   }
 
@@ -15,13 +15,13 @@ ReadFasta <- function(file, minrepeats = c(6,4,3,3,3), squishy = c(0,0,0,0,0)){#
     return(NULL)
   }
 
-  if(length(minrepeats) != 5 || class(minrepeats) != "numeric"){
-    print("minrepeats must be a numeric vector of length 5.")
+  if(length(minrep) != 1 || class(minrep) != "numeric"){
+    print("minrepeats must be a numeric vector of length 1.")
     return(NULL)
   }
 
-  if(length(squishy) != 5 || class(squishy) != "numeric"){
-    print("squishy must be a numeric vector of length 5.")
+  if(length(tolfac) != 5 || class(tolfac) != "numeric"){
+    print("squishy must be a numeric vector of length 1.")
     return(NULL)
   }
 
